a3be85ea763de2d12fe7d486ce38aa422c240d18
Valla Hallama <vhallama@gmail.com>
2021-10-20 10:08:02 +0300

    snapshot

--- src/graph_copy.c
+++ src/graph_copy.c
@@ -6,64 +6,63 @@
 /*   By: vhallama <vhallama@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 13:18:54 by vhallama          #+#    #+#             */
-/*   Updated: 2021/10/17 15:23:48 by vhallama         ###   ########.fr       */
+/*   Updated: 2021/10/20 09:57:48 by vhallama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
-void	copy_list(t_room **cpy, t_room **src, int rooms)
+void	copy_list(t_room **cpy, t_graph *graph)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	i = 0;
-	while (i < rooms)
+	while (i < graph->total_rooms)
 	{
-		cpy[i] = ft_malloc_safe(sizeof(t_room));
-		cpy[i]->ant = src[i]->ant;
-		cpy[i]->depth = src[i]->depth;
-		cpy[i]->input = src[i]->input;
-		cpy[i]->link = ft_malloc_safe(sizeof(t_room *) * src[i]->links);
-		cpy[i]->links = src[i]->links;
-		j = 0;
-		while (j < src[i]->links)
+		ft_memcpy(cpy[i], graph->list[i], sizeof(t_room));
+/* 		cpy[i]->depth = graph->list[i]->depth;
+		cpy[i]->input = graph->list[i]->input;
+		cpy[i]->output = graph->list[i]->output; */
+		if (graph->list[i] == graph->list[graph->start])
 		{
-			cpy[i]->link[j] = src[i]->link[j];
-			j++;
+			j = 0;
+			while (j < graph->list[i]->links)
+			{
+				ft_memcpy(cpy[i]->link[j], graph->list[i]->link[j], sizeof(t_room *));
+				j++;
+			}
 		}
-		cpy[i]->name = src[i]->name;
-		cpy[i]->output = src[i]->output;
-		cpy[i]->parent = src[i]->parent;
-		cpy[i]->visited = src[i]->visited;
 		i++;
 	}
 }
 
-t_room	**duplicate_list(t_room **src, int rooms)
+t_room	**duplicate_list(t_graph *graph)
 {
-	t_room	**cpy;
+	t_room	**dup;
 	int		i;
 	int		j;
 
-	cpy = ft_malloc_safe(sizeof(t_room *) * rooms);
+	dup = ft_malloc_safe(sizeof(t_room *) * graph->total_rooms);
 	i = 0;
-	while (i < rooms)
+	while (i < graph->total_rooms)
 	{
-		cpy[i] = ft_malloc_safe(sizeof(t_room));
-		cpy[i]->ant = src[i]->ant;
-		cpy[i]->depth = src[i]->depth;
-		cpy[i]->input = src[i]->input;
-		cpy[i]->link = ft_malloc_safe(sizeof(t_room *) * src[i]->links);
-		cpy[i]->links = src[i]->links;
-		j = -1;
-		while (++j < src[i]->links)
-			cpy[i]->link[j] = src[i]->link[j];
-		cpy[i]->name = src[i]->name;
-		cpy[i]->output = src[i]->output;
-		cpy[i]->parent = src[i]->parent;
-		cpy[i]->visited = src[i]->visited;
+		dup[i] = ft_malloc_safe(sizeof(t_room));
+		ft_memcpy(dup[i], graph->list[i], sizeof(t_room));
+		if (graph->list[i] == graph->list[graph->start])
+		{
+			dup[i]->link = ft_malloc_safe(sizeof(t_room *) * \
+			graph->list[i]->links);
+			j = 0;
+			while (j < graph->list[i]->links)
+			{
+				dup[i]->link[j] = ft_malloc_safe(sizeof(t_room *));
+				ft_memcpy(dup[i]->link[j], graph->list[i]->link[j], \
+				sizeof(t_room *));
+				j++;
+			}
+		}
 		i++;
 	}
-	return (cpy);
+	return (dup);
 }
